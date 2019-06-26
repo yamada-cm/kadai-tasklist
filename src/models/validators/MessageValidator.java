6eefd30781395bae771bf0e1340c5b8ba8887c3f
ma.ya <ma.ya@nakashima.co.jp>
2019-06-26 09:17:57 +0900

    kadai-tasklist

--- src/models/validators/MessageValidator.java
+++ src/models/validators/MessageValidator.java
@@ -27,7 +27,7 @@ public class MessageValidator
     {
         if(content==null||content.equals(""))
         {
-            return "メッセージを入力してください。";
+            return "タスクを入力してください。";
         }
 
         return "";
